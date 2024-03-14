15f2295e1d0b0e6fe42864fed82983d7af21290c
devwez <devwez@gmail.com>
2024-03-14 19:09:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# vite-vanilla-tailwindcss-template
+
+- **template of:** vite ^5 + vanila JS
+- **tailwindcss:** ^3
+
+## Started
+
+```bash
+npm install
+```
+
+---
+
+## Develop
+
+```bash
+npm run dev
+```
+
+---
+
+## Build
+
+```bash
+npm run build
+```
+
+---
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+    root,
+    optimizeDeps: {
+        include: ["linked-dep"],
+    },
+
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, "index.html"),
+            },
+        },
+    },
+});
